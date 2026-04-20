c849493ffdc83736a867556e18064dcfe6611013
XxRettilexX <abbona.menti2712@gmail.com>
2026-04-20 16:32:08 +0200

    modified:   backend/database/migrations/2026_04_20_142553_create_labels_table.php

--- backend/database/migrations/2026_04_20_142553_create_labels_table.php
+++ backend/database/migrations/2026_04_20_142553_create_labels_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('labels', function (Blueprint $table) {
             $table->id();
             $table->foreignId('lot_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete(); // Audit
             $table->string('qr_code_path')->nullable();
             $table->string('barcode')->nullable();
             $table->timestamps();
